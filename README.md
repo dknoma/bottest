29b0984244ed57bb379f0f046d186491d2766f68
Drew Noma <djknoma@gmail.com>
2018-08-17 17:24:15 -0700

    bot works now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# bottest
+# EMW Discord Bot
+
+## Installation
+
+In case the bot appears offline after starting it, the following library should be installed in the folder: ``npm install https://github.com/woor/discord.io/tarball/gateway_v6``
--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, { colorize: true });
 logger.level = 'debug';
 
-const bot = new Discord.Client({
+// Initialize Discord Bot
+var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
@@ -18,19 +19,20 @@ bot.on('ready', evt => {
 });
 
 bot.on('message', (user, userID, channelID, message, evt) => {
-    if(message.substring(0, 1) === '!')  {
-        var args = message.substring(1).split('');
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if(message.substring(0, 1) == '!')  {
+        var args = message.substring(1).split(' ');
         var cmd = args[0];
 
         args = args.splice(1);
-
         switch(cmd) {
             case 'hewwo':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'owo Hewwo owo'
+                    message: 'OwO _notices command_ hewwo'
                 });
-                break;
+            break;
         }
     }
 });

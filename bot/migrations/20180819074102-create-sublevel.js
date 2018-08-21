15feb9b1dde04fa0c907aeac7992762e24cab45d
Drew Noma <djknoma@gmail.com>
2018-08-20 20:18:18 -0700

    fixed model reference

--- bot/migrations/20180819074102-create-sublevel.js
+++ bot/migrations/20180819074102-create-sublevel.js
@@ -18,7 +18,7 @@ module.exports = {
       type: Sequelize.INTEGER,
       onDelete: 'CASCADE',
       references: {
-        model: 'Level',
+        model: 'Levels',
         key: 'id',
         as: 'levelId'
       }
